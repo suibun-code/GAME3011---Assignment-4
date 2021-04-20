79d19df857e6e8a6669f841e569224691e7d5a00
Ramin Amiri <zeuspsn.55555@gmail.com>
2021-04-19 23:27:54 -0400

    ok actually finished

--- Assets/Scripts/ButtonHackingSkill.cs
+++ Assets/Scripts/ButtonHackingSkill.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ButtonHackingSkill : MonoBehaviour
 {
+    public TMP_Text hackText;
+
     public void SetSkill(int skill)
     {
         HackingSkill.skill = skill;
 
         SceneManager.LoadScene("Level 1");
         HackingSkill.currentLevel = 1;
+    }
+
+    public void Start()
+    {
+        if (hackText != null)
+            hackText.SetText("Hacking skill: " + HackingSkill.skill);
+    }
+
+    public void Update()
+    {
+        if (hackText != null)
+            hackText.SetText("Hacking skill: " + HackingSkill.skill);
     }
 }
